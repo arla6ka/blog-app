fa3c328663fd74e944d32a9d7762e377219ec586
arla6ka <154040187+arla6ka@users.noreply.github.com>
2024-06-05 16:58:43 +0500

    Update page.js

--- test-1/app/page.js
+++ test-1/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-    <header className="bg-white">
+    <header className="bg-white min-h-screen">
         <div className="container mx-auto px-6 py-16 text-center">
           <h1 className="text-4xl font-bold text-gray-800 mb-2">Welcome to the Blog App</h1>
           <h2 className="text-2xl text-gray-600">
